c9a1118f6862e2eea3dc96bf1911110896807507
Sven Schoenung <sven.schoenung@gmail.com>
2018-08-04 09:54:03 +0200

    Add tests for CalendarEventService.getNextEvent()

--- src/test/java/io/jenkins/plugins/view/calendar/CalendarEventServiceTest.java
+++ src/test/java/io/jenkins/plugins/view/calendar/CalendarEventServiceTest.java
@@ -1,11 +1,18 @@
 package io.jenkins.plugins.view.calendar;
 
+import hudson.model.AbstractProject;
 import hudson.model.Run;
+import hudson.model.TopLevelItem;
+import hudson.triggers.Trigger;
+import hudson.triggers.TriggerDescriptor;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
@@ -13,6 +20,7 @@ import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
 
 @RunWith(Enclosed.class)
 public class CalendarEventServiceTest {
@@ -78,6 +86,46 @@ public class CalendarEventServiceTest {
             CalendarEvent nextEvent = new CalendarEventService().getNextEvent(event);
             assertThat(nextEvent, is(notNullValue()));
             assertThat(nextEvent.getBuild(), is(nextBuild));
+        }
+    }
+
+    public static class GetNextScheduledEventTests {
+        @Test
+        public void testIsNotAbstractProject() {
+            CalendarEvent event = mock(CalendarEvent.class);
+            when(event.getItem()).thenReturn(mock(TopLevelItem.class));
+
+            CalendarEvent nextScheduledEvent = new CalendarEventService().getNextScheduledEvent(event);
+            assertThat(nextScheduledEvent, is(nullValue()));
+        }
+
+        @Test
+        public void testHasNoNextScheduledEvent() {
+            AbstractProject project = mock(AbstractProject.class, withSettings().extraInterfaces(TopLevelItem.class));
+            CalendarEvent event = mock(CalendarEvent.class);
+            when(event.getItem()).thenReturn((TopLevelItem)project);
+
+            CalendarEvent nextScheduledEvent = new CalendarEventService().getNextScheduledEvent(event);
+            assertThat(nextScheduledEvent, is(nullValue()));
+        }
+
+        @Test
+        public void testHasNextScheduledEvent() {
+            Trigger trigger = mock(Trigger.class);
+            when(trigger.getSpec()).thenReturn("0 * * * *");
+
+            Map<TriggerDescriptor, Trigger> triggers = new HashMap<>();
+            triggers.put(mock(TriggerDescriptor.class), trigger);
+
+            AbstractProject project = mock(AbstractProject.class, withSettings().extraInterfaces(TopLevelItem.class));
+            when(project.getTriggers()).thenReturn(triggers);
+            when(project.getUrl()).thenReturn("project/url");
+
+            CalendarEvent event = mock(CalendarEvent.class);
+            when(event.getItem()).thenReturn((TopLevelItem)project);
+
+            CalendarEvent nextScheduledEvent = new CalendarEventService().getNextScheduledEvent(event);
+            assertThat(nextScheduledEvent, is(notNullValue()));
         }
     }
 }
