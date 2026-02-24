1df79c1719606c4c21be0c32dfb44758b1af2ae1
Daniele Caldarigi <danielecaldarigi05@gmail.com>
2026-02-24 11:25:58 +0100

    Added the option to change the results colors

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -38,6 +38,20 @@ const hashParamOptions = hashParams.parse(window.location.hash);
 var timeout = null;
 
 $(function() {
+  const root = document.documentElement;
+  if (CalendarViewOptions.resultsColors === 'yellow_blue') {
+    root.style.setProperty('--result-success-color', '#D4E300');
+    root.style.setProperty('--result-failure-color', '#001FD4');
+    root.style.setProperty('--result-success-selected-color', '#E6F500');
+    root.style.setProperty('--result-failure-selected-color', '#001FFF');
+  }
+  else{
+    root.style.setProperty('--result-success-color', '#00B80E');
+    root.style.setProperty('--result-failure-color', '#CF0000');
+    root.style.setProperty('--result-success-selected-color', '#00E610');
+    root.style.setProperty('--result-failure-selected-color', '#FF0000');
+  }
+
   $('#calendar-view').fullCalendar({
     events: 'events',
     defaultView: hashParamOptions['view'] || CalendarViewOptions.defaultView,
